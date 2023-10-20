8f44f5d127a5063b909d18a1f14ffff51a35e223
kekcoe <kekcoe@gmail.com>
2023-10-20 07:25:36 +0300

    Was created bot with client for see weather

--- src/main/java/ru/kekcoe/feignweather/weatherbot/WeatherBotConfig.java
+++ src/main/java/ru/kekcoe/feignweather/weatherbot/WeatherBotConfig.java
@@ -1,11 +1,9 @@
 package ru.kekcoe.feignweather.weatherbot;
 
-import lombok.var;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.TelegramBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
